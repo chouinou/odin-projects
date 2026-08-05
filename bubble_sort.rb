3973b5dcc9cba3f8bfccac9303a8e7c7efc69530
chouinou <guerreiromain@proton.me>
2026-08-05 15:36:31 +0200

    initial commit

---
+++ bubble_sort.rb
@@ -0,0 +1,30 @@
+
+def bubble_sort(array)
+    
+    not_sorted = true
+    
+    while not_sorted 
+
+        i = 0
+        not_sorted = false
+        
+        while i < (array.length - 1)
+                
+            if array[i] > array[i+1]
+                array [i], array[i+1] = array[i+1], array[i]
+                not_sorted = true
+            end
+            
+            i += 1
+
+            puts array
+        
+        end
+               
+    end
+
+    puts array
+
+end
+
+bubble_sort([4,3,78,2,0,2,0,12,11,85,86,32,54,66,66,512,354684,32154,3,78,2,0,2,0,12,11,85,86,32])
